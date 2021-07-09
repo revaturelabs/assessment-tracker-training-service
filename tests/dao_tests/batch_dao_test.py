7395029eb387ce334f1cc586b70d0dac8104501a
Bishwo Purkuti <bishwopurkutihhs@gmail.com>
2021-07-08 17:30:15 -0700

    Uncommented some tests

--- tests/dao_tests/batch_dao_test.py
+++ tests/dao_tests/batch_dao_test.py
@@ -31,41 +31,41 @@ class DAOTests(unittest.TestCase):
                 with conn.cursor() as cursor:
                     test_batch = batch_dao.create_batch(cursor, test_batch)
 
-    # def test_get_all_batches_by_year(self):
-    #     with conn:
-    #         with conn.cursor() as cursor:
-    #             batches = BatchDAOImpl().get_all_batches_by_year(
-    #                 cursor, 1, 2020)
-    #             self.assertTrue(batches)
-    #         conn.rollback()
-    #
-    # def test_get_all_batches_by_year_fail(self):
-    #     with conn:
-    #         with conn.cursor() as cursor:
-    #             self.assertFalse(BatchDAOImpl().get_all_batches_by_year(
-    #                 cursor, 1, 1))
-    #         conn.rollback()
-    #
-    # def test_get_batch_by_id(self):
-    #     with conn:
-    #         with conn.cursor() as cursor:
-    #             self.assertTrue(BatchDAOImpl().get_batch_by_id(cursor, 1))
-    #         conn.rollback()
-    #
-    # def test_get_batch_by_id_fail(self):
-    #     with conn:
-    #         with conn.cursor() as cursor:
-    #             try:
-    #                 BatchDAOImpl().get_batch_by_id(cursor, 10000)
-    #                 assert False
-    #             except ResourceNotFound:
-    #                 assert True
-    #         conn.rollback()
-    #
-    # def test_search(self):
-    #     with conn:
-    #         with conn.cursor() as cursor:
-    #             batches = BatchDAOImpl().search_for_batch(cursor, 1, "py")
-    #             print(batches)
-    #             assert batches
-    #         conn.rollback()
+    def test_get_all_batches_by_year(self):
+        with conn:
+            with conn.cursor() as cursor:
+                batches = BatchDAOImpl().get_all_batches_by_year(
+                    cursor, 1, 2020)
+                self.assertTrue(batches)
+            conn.rollback()
+
+    def test_get_all_batches_by_year_fail(self):
+        with conn:
+            with conn.cursor() as cursor:
+                self.assertFalse(BatchDAOImpl().get_all_batches_by_year(
+                    cursor, 1, 1))
+            conn.rollback()
+
+    def test_get_batch_by_id(self):
+        with conn:
+            with conn.cursor() as cursor:
+                self.assertTrue(BatchDAOImpl().get_batch_by_id(cursor, 1))
+            conn.rollback()
+
+    def test_get_batch_by_id_fail(self):
+        with conn:
+            with conn.cursor() as cursor:
+                try:
+                    BatchDAOImpl().get_batch_by_id(cursor, 10000)
+                    assert False
+                except ResourceNotFound:
+                    assert True
+            conn.rollback()
+
+    def test_search(self):
+        with conn:
+            with conn.cursor() as cursor:
+                batches = BatchDAOImpl().search_for_batch(cursor, 1, "py")
+                print(batches)
+                assert batches
+            conn.rollback()
