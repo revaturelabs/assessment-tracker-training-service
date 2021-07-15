f28d2ddcbbaa45abf574c3fc10a1e68820921a92
Areesh Lalani <alalani@iastate.edu>
2021-07-15 15:37:33 -0500

    trainer controller

--- controllers/trainer_controller.py
+++ controllers/trainer_controller.py
@@ -1,22 +1,76 @@
 from flask import request, jsonify
 from flask_restx import Api, Resource
 
+from flask_restx import Api, Resource
 from exceptions.resource_not_found import ResourceNotFound
 from services.trainer_service import TrainerService
 from utils.json_tool import convert_list_to_json
+
+
 
 INVALID_ID_ERROR = "Not a valid ID or No such batch exist with this ID"
 
 
 def route(api):
 
-    # @app.route("/trainers", methods=["POST"])
-    # def login():
-    #     try:
-    #         email = request.json["email"]
-    #         return jsonify(TrainerService.login(email).json())
-    #     except ResourceNotFound as r:
-    #         return r.message, 404
+    @api.route('/trainers')
+    @api.response(404, "No user with those credentials")
+    class Login(Resource):
+        def post(self):
+            try:
+                email = request.json["email"]
+                return jsonify(TrainerService.login(email).json())
+            except ResourceNotFound as r:
+                return r.message, 404
+
+
+
+    @api.route('/trainers/<trainer_id>')
+    @api.param('trainer_id', "Trainer ID")
+    @api.response(400, "Bad Request")
+    @api.response(404, "No Trainer with that ID")
+    class GetTrainerById(Resource):
+        def get(self, trainer_id):
+            try:
+                return jsonify(TrainerService.get_trainer_by_id(int(trainer_id)).json())
+            except ValueError:
+                return INVALID_ID_ERROR, 400  # Bad Request
+            except ResourceNotFound as r:
+                return r.message, 404
+
+
+    @api.route('/batches/<batch_id>/trainers')
+    @api.param('batch_id', "Batch ID of the batch you want trainer")
+    @api.response(400, "Bad Request")
+    @api.response(404, 'No trainers associated with that Batch')
+    class GetTrainersInBatch(Resource):
+        def get(self, batch_id):
+            try:
+                trainers = TrainerService.get_trainers_in_batch(int(batch_id))
+            except ValueError:
+                return INVALID_ID_ERROR, 400  # Bad Request
+            except ResourceNotFound as r:
+                return r.message, 404
+            trainers_as_json = convert_list_to_json(trainers)
+            return jsonify(trainers_as_json)
+
+
+
+    @api.route('/years')
+    @api.response(400, "Invalid ID")
+    class GetYearsForTrainer(Resource):
+        def get(self):
+            trainer_id = request.args.get("trainerId")
+            if trainer_id is not None:
+                try:
+                    return jsonify(TrainerService.get_years_for_trainer(int(trainer_id)))
+                except ValueError:
+                    return INVALID_ID_ERROR, 400  # Bad Request
+            else:
+                return jsonify([])
+
+
+
 
     @api.route('/trainers')
     @api.response(404, "No user with those credentials")
@@ -28,34 +82,3 @@ def route(api):
             except ResourceNotFound as r:
                 return r.message, 404
 
-    # @app.route("/trainers/<trainer_id>", methods=["GET"])
-    # def get_trainer_by_id(trainer_id):
-    #     try:
-    #         return jsonify(TrainerService.get_trainer_by_id(int(trainer_id)).json())
-    #     except ValueError:
-    #         return INVALID_ID_ERROR, 400  # Bad Request
-    #     except ResourceNotFound as r:
-    #         return r.message, 404
-
-    # @app.route("/batches/<batch_id>/trainers", methods=["GET"])
-    # def get_trainers_by_batch_id(batch_id):
-    #     try:
-    #         trainers = TrainerService.get_trainers_in_batch(int(batch_id))
-    #     except ValueError:
-    #         return INVALID_ID_ERROR, 400  # Bad Request
-    #     except ResourceNotFound as r:
-    #         return r.message, 404
-    #     trainers_as_json = convert_list_to_json(trainers)
-    #     return jsonify(trainers_as_json)
-    #
-    # # /years?trainer_id=<trainer_id>
-    # @app.route("/years", methods=["GET"])
-    # def get_years_for_trainer():
-    #     trainer_id = request.args.get("trainerId")
-    #     if trainer_id is not None:
-    #         try:
-    #             return jsonify(TrainerService.get_years_for_trainer(int(trainer_id)))
-    #         except ValueError:
-    #             return INVALID_ID_ERROR, 400  # Bad Request
-    #     else:
-    #         return jsonify([])
